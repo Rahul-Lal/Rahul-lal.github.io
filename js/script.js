fb0cc1a608b20d5d49edf5cc41896215f65c9e8a
Rahul Lal <Rahul21Lal@gmail.com>
2019-04-23 11:11:23 +1200

    Fixed nav bar, blog links, and page format for blog so far

--- js/script.js
+++ js/script.js
@@ -1,50 +1,12 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+/*
+  For Navigation bar
+*/
 
-function plusSlides(n)
-{
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n)
-{
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n)
-{
-    var i = 0; 
-
-  var slides = document.getElementsByClassName("fadingSlide");
-
-  if (n > slides.length)
-  {
-      slideIndex = 1;
-  }
-  else if (n < 1)
-  {
-      slideIndex = slides.length;
-  }
-  
-  for (; i < slides.length; i++)
-  {
-      slides[i].style.display = "none"; 
-  }
-
-  slides[slideIndex-1].style.display = "block";
-}
+window.onscroll = function() {myFunction()}; // When the user scrolls the page, execute myFunction 
 
 
-// For Navigation bar
-
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("header");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var navbar = document.getElementById("navbar"); // Get the navbar
+var sticky = navbar.offsetTop; // Get the offset position of the navbar
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction()
