8d3ea71e05d622297f4c51a42ee58c371e86e32a
Rahul Lal <Rahul21Lal@gmail.com>
2025-09-10 09:35:38 +1200

    To Be Continued
    With Navbar

--- js/script.js
+++ js/script.js
@@ -1,63 +1,6 @@
-/*
-  For Navigation bar
-*/
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav-links');
 
-window.onscroll = function() {myFunction()}; // When the user scrolls the page, execute myFunction 
-
-
-var navbar = document.getElementById("navbar"); // Get the navbar
-var sticky = navbar.offsetTop; // Get the offset position of the navbar
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction()
-{
-  if (window.pageYOffset >= sticky)
-  {
-    navbar.classList.add("sticky")
-  }
-  else
-  {
-    navbar.classList.remove("sticky");
-  }
-}
-
-function warnUserOfResume()
-{
-  alert("You have downloaded a copy of my resume\n\n Enjoy!");
-}
-
-
-function testSum()
-{
-  var expected = 7;
-  var actual = sum(5, 2)
-  
-  if (actual != expected)
-  {
-    console.log("It's broken..");
-  }
-  else
-  {
-    console.log("It works!");
-  }
-}
-
-
-function secondTestSum(hopefullyTrue)
-{
-  if(typeof hopefullyTrue === "boolean")
-  {
-    if (hopefullyTrue === true)
-    {
-      console.log("It works!");
-    }
-    else
-    {
-      console.log("It's broken..");
-    }
-  }
-  else
-  {
-    console.log("IT HAS TO BE A BOOLEAN!!!!");
-  }
-}
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
