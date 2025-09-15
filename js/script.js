4d48094c428bf6b9b1a635a909c3ea1a86301454
Rahul Lal <Rahul21Lal@gmail.com>
2025-09-15 13:25:19 +1200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,11 +11,13 @@ fetch("projects.json")
       card.innerHTML = `
         <img src="${project.image}" alt="${project.title}">
         <h3>${project.title}</h3>
-        <p>${project.description}</p>
-        <a href="${project.link}" target="_blank">View Project</a>
+        <p>${project.description}
+        <a href="${project.link}" target="_blank">View Project</a> | <a href="${project.github}" target="_blank">View Github</a>
+        </p>
       `;
 
       container.appendChild(card);
     });
   })
   .catch(error => console.error("Error loading projects:", error));
+
